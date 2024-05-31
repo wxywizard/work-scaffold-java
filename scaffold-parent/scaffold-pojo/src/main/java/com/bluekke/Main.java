392f5c73a71d2194799a05876b8a32f56d10331f
wangxy <wangxuyang16@crdigital.com.cn>
2024-05-31 15:56:12 +0800

    add base project

---
+++ scaffold-parent/scaffold-pojo/src/main/java/com/bluekke/Main.java
@@ -0,0 +1,13 @@
+package com.bluekke;
+
+/**
+ * Description for this class
+ *
+ * @Author will
+ * @create 2024/5/31 15:24
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
