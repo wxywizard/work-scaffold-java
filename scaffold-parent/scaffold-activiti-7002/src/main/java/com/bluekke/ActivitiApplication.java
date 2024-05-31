fbdc9881fc1821ed93c096470fd2931ca1639727
wangxy <wangxuyang16@crdigital.com.cn>
2024-05-31 17:01:06 +0800

    add base project

---
+++ scaffold-parent/scaffold-activiti-7002/src/main/java/com/bluekke/ActivitiApplication.java
@@ -0,0 +1,18 @@
+package com.bluekke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description for this class
+ *
+ *@Author will
+ *@create 2024/5/31 16:46
+ */
+@SpringBootApplication
+public class ActivitiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiApplication.class, args);
+    }
+
+}
---
+++ scaffold-parent/scaffold-auth-7001/src/main/java/com/wizard/scaffold/AuthApplication.java
@@ -0,0 +1,17 @@
+package com.wizard.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description for this class
+ *
+ * @Author will
+ * @create 2024/5/31 16:53
+ */
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
